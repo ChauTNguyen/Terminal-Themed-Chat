0ad3147474659b041dcba728b762bab1b4982854
Chau Nguyen <chautnguyen96@gmail.com>
2016-08-21 17:47:01 -0700

    fix tabs->spaces

--- chat.js
+++ chat.js
@@ -9,106 +9,106 @@ var clients = [];
 var users = {};
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static(__dirname));
 
 io.on('connection', function (socket) {
-	clients.push(socket);
-	io.emit('event message', "Anon has joined the room.");
+  clients.push(socket);
+  io.emit('event message', "Anon has joined the room.");
 
-	socket.on('event message', function (msg) {
-		io.emit('event message', msg);
-	});
+  socket.on('event message', function (msg) {
+    io.emit('event message', msg);
+  });
 
-	socket.on('chat message', function (msg) {
-		var nickname = users[socket.id];
-		if (nickname === undefined) {
-			socket.broadcast.emit('chat message', "anon@" + socket.id.substring(2, socket.id.length) + ": " + msg);
-		} else {
-			socket.broadcast.emit('chat message', nickname + ": " + msg);
-		}
-	});
+  socket.on('chat message', function (msg) {
+    var nickname = users[socket.id];
+    if (nickname === undefined) {
+      socket.broadcast.emit('chat message', "anon@" + socket.id.substring(2, socket.id.length) + ": " + msg);
+    } else {
+      socket.broadcast.emit('chat message', nickname + ": " + msg);
+    }
+  });
 
-	socket.on('disconnect', function () {
-		var index = clients.indexOf(socket);
-		if (index !== -1) {
-			clients.splice(index, 1);
-			var nickname = users[socket.id];
-			if (nickname === undefined) {
-				io.emit('chat message', "anon@" + socket.id.substring(2, socket.id.length) + " has disconnected.");
-			} else {
-				io.emit('event message', users[socket.id] + " has disconnected.");
-			}
-			delete users[socket.id];
-		}
-	});
+  socket.on('disconnect', function () {
+    var index = clients.indexOf(socket);
+    if (index !== -1) {
+      clients.splice(index, 1);
+      var nickname = users[socket.id];
+      if (nickname === undefined) {
+        io.emit('chat message', "anon@" + socket.id.substring(2, socket.id.length) + " has disconnected.");
+      } else {
+        io.emit('event message', users[socket.id] + " has disconnected.");
+      }
+      delete users[socket.id];
+    }
+  });
 
-	socket.on('join', function (name) {
-		users[socket.id] = name;
-		io.emit('event message', "anon@" + socket.id.substring(2, socket.id.length) + " has become " + name + ".");
-	});
+  socket.on('join', function (name) {
+    users[socket.id] = name;
+    io.emit('event message', "anon@" + socket.id.substring(2, socket.id.length) + " has become " + name + ".");
+  });
 
-	socket.on('ls', function () {
-		var keys = [];
-		for (var key in users) {
-			if (users.hasOwnProperty(key)) {
-				keys.push(key);
-			}
-		}
+  socket.on('ls', function () {
+    var keys = [];
+    for (var key in users) {
+      if (users.hasOwnProperty(key)) {
+        keys.push(key);
+      }
+    }
 
-		var index = clients.indexOf(socket);
-		clients[index].emit('event message', "There are " + keys.length + " users" +
-			" & " + clients.length + " clients."
-		);
+    var index = clients.indexOf(socket);
+    clients[index].emit('event message', "There are " + keys.length + " users" +
+      " & " + clients.length + " clients."
+    );
 
-		for (var i = 0; i < keys.length; ++i) {
-			clients[index].emit('event message', users[keys[i]]);
-		}
-	});
+    for (var i = 0; i < keys.length; ++i) {
+      clients[index].emit('event message', users[keys[i]]);
+    }
+  });
 
-	socket.on('pm', function (pm_target, pm_message) {
-		if (users[socket.id] === undefined) {
-			socket.emit('event message', "You must have a nickname in order to use /pm.");
-		} else {
-			var targetUserID;
+  socket.on('pm', function (pm_target, pm_message) {
+    if (users[socket.id] === undefined) {
+      socket.emit('event message', "You must have a nickname in order to use /pm.");
+    } else {
+      var targetUserID;
 
-			for (var prop in users) {
-				if (users.hasOwnProperty(prop)) {
-					if (users[prop] == pm_target) {
-						targetUserID = prop;
-					}
-				}
-			}
+      for (var prop in users) {
+        if (users.hasOwnProperty(prop)) {
+          if (users[prop] == pm_target) {
+            targetUserID = prop;
+          }
+        }
+      }
 
-			if (targetUserID !== undefined) {
-				var indexOfSender = clients.indexOf(socket);
+      if (targetUserID !== undefined) {
+        var indexOfSender = clients.indexOf(socket);
 
-				var indexOfRecipient;
+        var indexOfRecipient;
 
-				for (var i = 0; i < clients.length; ++i) {
-					if (clients[i].id == targetUserID) {
-						indexOfRecipient = i;
-						break;
-					}
-				}
+        for (var i = 0; i < clients.length; ++i) {
+          if (clients[i].id == targetUserID) {
+            indexOfRecipient = i;
+            break;
+          }
+        }
 
-				if (clients[indexOfRecipient] !== socket) {
-					socket.emit('chat message', "To [" + users[clients[indexOfRecipient].id] + "] - " + pm_message);
-					clients[indexOfRecipient].emit(
-						'chat message',
-						"From [" + users[clients[indexOfSender].id] + "] - " + pm_message);
-				} else {
-					socket.emit('event message', "You cannot message yourself.");
-				}
-			} else {
-				socket.emit('event message', "Nickname not found.");
-			}
-		}
-	});
+        if (clients[indexOfRecipient] !== socket) {
+          socket.emit('chat message', "To [" + users[clients[indexOfRecipient].id] + "] - " + pm_message);
+          clients[indexOfRecipient].emit(
+            'chat message',
+            "From [" + users[clients[indexOfSender].id] + "] - " + pm_message);
+        } else {
+          socket.emit('event message', "You cannot message yourself.");
+        }
+      } else {
+        socket.emit('event message', "Nickname not found.");
+      }
+    }
+  });
 });
 
 http.listen(3000, function () {
-	console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
